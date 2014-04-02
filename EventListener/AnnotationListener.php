ba9dafc5e1e9333bbfc2f1a789115e10ba42b87e
Mario Young <maye.co@gmail.com>
2014-04-02 01:15:55 -0300

    allow array annotation

--- EventListener/AnnotationListener.php
+++ EventListener/AnnotationListener.php
@@ -24,35 +24,67 @@ class AnnotationListener implements EventSubscriberInterface
 
     public function onKernelController(FilterControllerEvent $event)
     {
-
-        if (!is_array($controller = $event->getController())) {
+        if (!is_array($controller = $event->getController()))
+        {
             return;
         }
 
-        foreach($event->getRequest()->attributes->all() as $atribute){
+        foreach($event->getRequest()->attributes->all() as $atribute)
+        {
+            if(is_array($atribute))
+            {
+                foreach($atribute as $atribute_array)
+                {
 
-            if($atribute instanceof ContainerAwareInterface){
-                $atribute->setContainer($this->container);
+                    if($atribute_array instanceof ContainerAwareInterface){
+                        $atribute_array->setContainer($this->container);
+                    }
+
+                    if($atribute_array instanceof ExecutableAnnotationControllerInterface){
+                        $atribute_array->executeOnController($event);
+                    }
+                }
             }
+            else
+            {
+                if($atribute instanceof ContainerAwareInterface){
+                    $atribute->setContainer($this->container);
+                }
 
-            if($atribute instanceof ExecutableAnnotationControllerInterface){
-                $atribute->executeOnController($event);
+                if($atribute instanceof ExecutableAnnotationControllerInterface){
+                    $atribute->executeOnController($event);
+                }
             }
-
         }
     }
 
     public function onKernelView(GetResponseForControllerResultEvent $event)
     {
+        foreach($event->getRequest()->attributes->all() as $atribute)
+        {
+            if(is_array($atribute))
+            {
+                foreach($atribute as $atribute_array)
+                {
 
-        foreach($event->getRequest()->attributes->all() as $atribute){
+                    if($atribute_array instanceof ContainerAwareInterface){
+                        $atribute_array->setContainer($this->container);
+                    }
 
-            if($atribute instanceof ContainerAwareInterface){
-                $atribute->setContainer($this->container);
+                    if($atribute_array instanceof ExecutableAnnotationControllerInterface){
+                        $atribute_array->executeOnController($event);
+                    }
+                }
             }
+            else
+            {
+                if($atribute instanceof ContainerAwareInterface){
+                    $atribute->setContainer($this->container);
+                }
 
-            if($atribute instanceof ExecutableAnnotationViewInterface){
-                $atribute->executeOnView($event);
+                if($atribute instanceof ExecutableAnnotationViewInterface){
+                    $atribute->executeOnView($event);
+                }
             }
 
         }
